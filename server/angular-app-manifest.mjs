e78d92221de4b6a0b5a63db27b3c800c7590c8b0
Amina Kifayat <amnakifayat10gujrat@gmail.com>
2025-07-29 01:10:08 +0500

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -20,11 +20,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 521, hash: '334752a603342d1c2dfd054dde231e06dca3954b9e266333fb79a85160e52728', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1034, hash: '55a380b6ddbabb8e8ff87663c7d93c780ea1e0c99ffa6205dfb9d6a774d781a5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 4476, hash: '04f823b29d4fd8804a780669edaa8a31662fa0d32d9ef61ae75c30d91b5834e5', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 9968, hash: '4780fcb18241a6123acfde214e39b295beecb836a46b6c632cee0834ae7354b3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'docs/index.html': {size: 11970, hash: 'b668f2c0a704a58f1f7d99c7f05da5ae209f268e29530f14fbea32b2b1870b65', text: () => import('./assets-chunks/docs_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 521, hash: 'c4ffb2b4285f80a030d6800da30d7ae1b65829856c2d84bc7e64f50b32711ff2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1034, hash: 'c5ad177ab24add212461d5ee1f9fedad0e118cdd834ff2232a16244b6167ce95', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 4476, hash: '86c7b349c6f68c95062b8fad363c0e60c2ddac41d4f73c6c45f496429571a7bf', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 10054, hash: '7100f1aefc23988bf1d989263e9293a7a6953bfd8fa10807f4ff46dd05141722', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'docs/index.html': {size: 11970, hash: 'a165573576f293d2c012cba329994d112db71b8ac52d1dfb98d7827dfa483ff5', text: () => import('./assets-chunks/docs_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
